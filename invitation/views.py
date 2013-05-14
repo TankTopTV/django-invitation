605b3e95847adf6cdc768d9a45176fa9a0692b91
Liz Rice <liz@lizrice.com>
2013-05-14 17:24:22 +0100

    Tell the user what's wrong with their invitation code if it's not valid

--- invitation/views.py
+++ invitation/views.py
@@ -10,10 +10,10 @@ if getattr(settings, 'INVITATION_USE_ALLAUTH', False):
     from allauth.socialaccount.views import signup as allauth_signup
     from allauth.socialaccount.forms import SignupForm as RegistrationForm
     registration_template = 'accounts/signup.html'
-    
+
     def registration_register(request, backend, success_url, form_class, disallowed_url, template_name, extra_context):
         return allauth_signup(request, template_name=template_name)
-else:        
+else:
     from registration.views import register as registration_register
     from registration.forms import RegistrationForm
     registration_template = 'registration/registration_form.html'
@@ -23,15 +23,30 @@ from invitation.forms import InvitationKeyForm
 from invitation.backends import InvitationBackend
 
 is_key_valid = InvitationKey.objects.is_key_valid
+get_key = InvitationKey.objects.get_key
+
 remaining_invitations_for_user = InvitationKey.objects.remaining_invitations_for_user
 
 def invited(request, invitation_key=None, extra_context=None):
     if getattr(settings, 'INVITE_MODE', False):
+        extra_context = extra_context is not None and extra_context.copy() or {}
         if invitation_key and is_key_valid(invitation_key):
             template_name = 'invitation/invited.html'
         else:
+            if invitation_key:
+                extra_context.update({'invitation_key': invitation_key})
+                ik = get_key(invitation_key)
+                if ik:
+                    if ik.key_expired():
+                        extra_context.update({'expired_key': True})
+                    else:
+                        assert ik.uses_left == 0
+                        extra_context.update({'no_uses_left_key': True})
+                else:
+                    extra_context.update({'invalid_key': True})
+            else:
+                extra_context.update({'no_key': True})
             template_name = 'invitation/wrong_invitation_key.html'
-        extra_context = extra_context is not None and extra_context.copy() or {}
         extra_context.update({'invitation_key': invitation_key})
         request.session['invitation_key'] = invitation_key
         return direct_to_template(request, template_name, extra_context)
